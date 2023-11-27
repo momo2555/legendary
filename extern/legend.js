62b4be301af91404939efb67b126ee39098816fd
Mohamed Al Glawi <shadowrapplique@gmail.com>
2023-11-28 00:28:21 +0100

    add log

--- extern/legend.js
+++ extern/legend.js
@@ -104,6 +104,7 @@
                }else if(type=="data_exchange") {
                     this.exeDataListeners(data.header.from, data.data);
                }else if(type=="device_event") {
+                console.log("device_event received");
                     this.execDeviceEventListeners(data.header.from, data.event);
                }else if(type=="device_data") {
                     
